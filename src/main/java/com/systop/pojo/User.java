8b8083201af44e3a48b14016e2dd1546d7f5dfe7
Shrimp_study <3177336507@qq.com>
2020-04-14 22:04:54 +0800

    第一次提交测试

---
+++ src/main/java/com/systop/pojo/User.java
@@ -0,0 +1,16 @@
+package com.systop.pojo;
+
+/**
+ * @Package: com.systop.pojo
+ * @ClassName: User
+ * @version: 1.0.0
+ * @description: TODO
+ * @author: Shrimp
+ * @create: 2020-04-14 22:04
+ **/
+
+public class User {
+
+
+
+}
